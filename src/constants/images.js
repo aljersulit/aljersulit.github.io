9d69007f5455da2e5935ef7e1431ada34d7be6ec
Aldrin Sulit <aljersulit12@gmail.com>
2023-02-13 10:27:38 +0800

    adjusted project tags

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,37 +1,39 @@
-import bgIMG from '../assets/bgIMG.png';
-import bgWhite from '../assets/bgWhite.png';
-import css from '../assets/css.png';
-import email from '../assets/email.png';
-import git from '../assets/git.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import logo from '../assets/logo.png';
-import mobile from '../assets/mobile.png';
-import nb from '../assets/nb.png';
-import profile from '../assets/profile.png';
-import python from '../assets/python.png';
-import react from '../assets/react.png';
-import sass from '../assets/sass.png';
-import circle from '../assets/circle.svg';
-import bg from '../assets/bg.png';
-import ajs from '../assets/ajs_logo.png';
+import bgIMG from "../assets/bgIMG.png";
+import bgWhite from "../assets/bgWhite.png";
+import css from "../assets/css.png";
+import email from "../assets/email.png";
+import git from "../assets/git.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import logo from "../assets/logo.png";
+import mobile from "../assets/mobile.png";
+import nb from "../assets/nb.png";
+import profile from "../assets/profile.png";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import sass from "../assets/sass.png";
+import circle from "../assets/circle.svg";
+import bg from "../assets/bg.png";
+import ajs from "../assets/ajs_logo.png";
 
-export default {
-    bgIMG,
-    bgWhite,
-    circle,
-    css,
-    email,
-    git,
-    html,
-    javascript,
-    logo,
-    mobile,
-    nb,
-    profile,
-    python,
-    react,
-    sass,
-    bg,
-    ajs
-}
+const images = {
+  bgIMG,
+  bgWhite,
+  circle,
+  css,
+  email,
+  git,
+  html,
+  javascript,
+  logo,
+  mobile,
+  nb,
+  profile,
+  python,
+  react,
+  sass,
+  bg,
+  ajs,
+};
+
+export default images;
